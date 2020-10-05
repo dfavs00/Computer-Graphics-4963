1d05765d8b1717270d51a3f9745bc695be4a8faa
favatd <greenbaypackersfam08@gmail.com>
2020-10-05 17:50:42 -0400

    Cube Example from the book. Testing stuff out

---
+++ Code/Learning/Cube/Cube/Utils.h
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gl/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Utils {
+public:
+	static string readShaderSource(const char* filePath);
+	static GLuint createShaderProgram(const char* vShaderFile, const char* fShaderFile);
+	static void printShaderLog(GLuint shader);
+	static void printProgramLog(int prog);
+	static bool checkOpenGLError();
+};
+
+/*
+*Read a Shader in from a file and return the file content as a string
+*/
+string Utils::readShaderSource(const char* filePath) {
+	string content;
+	ifstream fileStream(filePath, ios::in);
+	string line = "";
+	// while not at the end of the file
+	// build line by line
+	while (!fileStream.eof()) {
+		getline(fileStream, line);
+		content.append(line + "\n");
+	}
+	fileStream.close();
+	return content;
+}
+
+
+// create a shader program given cShaderFile and fShaderFile
+GLuint Utils::createShaderProgram(const char* vShaderFile, const char* fShaderFile) {
+
+	string vertShaderStr = readShaderSource(vShaderFile);
+	string fragShaderStr = readShaderSource(fShaderFile);
+
+	const char* vshaderSource = vertShaderStr.c_str();
+	const char* fshaderSource = fragShaderStr.c_str();
+
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(vShader, 1, &vshaderSource, NULL);
+	glShaderSource(fShader, 1, &fshaderSource, NULL);
+	glCompileShader(vShader);
+	glCompileShader(fShader);
+
+	GLuint vfProgram = glCreateProgram();
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+	glLinkProgram(vfProgram);
+
+	return vfProgram;
+}
+
+/* ================utilities for debugging below===================*/
+
+/*
+	print shader log for GLuint shader
+*/
+void Utils::printShaderLog(GLuint shader) {
+	int len = 0;
+	int chWrittn = 0;
+	char* log;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0) {
+		log = (char*)malloc(len);
+		glGetShaderInfoLog(shader, len, &chWrittn, log);
+		cout << "Shader Info Log:" << log << endl;
+		free(log);
+	}
+}
+
+/*
+	print program log for a shader program
+*/
+void Utils::printProgramLog(int prog) {
+	int len = 0;
+	int chWrittn = 0;
+	char* log;
+	glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0) {
+		log = (char*)malloc(len);
+		glGetProgramInfoLog(prog, len, &chWrittn, log);
+		cout << "Program Info Log:" << log << endl;
+		free(log);
+	}
+}
+
+/*
+	check for any openGL errors
+*/
+bool Utils::checkOpenGLError() {
+	bool foundError = false;
+	int glErr = glGetError();
+	while (glErr != GL_NO_ERROR) {
+		cout << "glError:" << glErr << endl;
+		foundError = true;
+		glErr = glGetError();
+	}
+	return foundError;
+}
---
+++ Code/Learning/Cube/Cube/main.cpp
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gl/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include "Utils.h"
+
+using namespace std;
+
+#define numVAOs 1
+#define numVBOs 2
+
+float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocZ;
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+
+// variables used in display to save time during rendering
+GLuint myLoc, projLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat, mMat, mvMat, rotMat;
+
+// Eventually might be able to pass a file into here or something
+void setupVerticies(void) {
+	float vertexPositions[108] = {
+		-1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f,
+		1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f,
+		1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+		-1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+		-1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+		-1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+		-1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,
+		-1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f
+	};
+
+	glGenVertexArrays(1, vao);
+	glBindVertexArray(vao[0]);
+	glGenBuffers(numVBOs, vbo);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+}
+
+
+
+void init(GLFWwindow* window) {
+
+	renderingProgram =  Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
+	cameraX = 0.0f, cameraY = 0.0f, cameraZ = 8.0f;
+	cubeLocX = 0.0f, cubeLocY = -2.0f, cubeLocZ = 0.0f;
+	setupVerticies();
+}
+
+
+void display(GLFWwindow* window, double currentTime) {
+
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glUseProgram(renderingProgram);
+	
+	// get uniform variables for MV and projection matricies
+	GLuint mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+	GLuint projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+	GLuint projRot = glGetUniformLocation(renderingProgram, "rot_matrix");
+	
+	// build perspective matrix
+	glfwGetFramebufferSize(window, &width, &height);
+	aspect = (float)width / (float)height;
+	//angle is in radians (60.0f degrees == 1.0472f radians)
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+	
+	// Build View Matrix
+	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ) ); // reverse the camera coordinates
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ) );  // position of the cube
+	mvMat = vMat * mMat;
+	rotMat = glm::rotate(glm::mat4(1.0f), (float)(currentTime), glm::vec3(1,1,1));
+
+	//put matricies into correct uniform variables in rendering program
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+	glUniformMatrix4fv(projRot, 1, GL_FALSE, glm::value_ptr(rotMat));
+
+	// Vertex Buffer Object gets associated with the correct vertex 
+	// attribute in the vertex shader
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	// adjust OpenGL setting to draw model
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+}
+
+
+int main(void) {
+
+	//glfwInit initializes the GLFW library
+	if (!glfwInit()) { exit(EXIT_FAILURE); }
+	//Tells the program what GLFW version that is being used (in this case 4.3)
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	//Make window
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Cube", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	//glewInit initializes the GLEW library
+	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+
+	init(window);
+
+	//Loop runs until the window is closed
+	while (!glfwWindowShouldClose(window)) {
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+
+}
+
